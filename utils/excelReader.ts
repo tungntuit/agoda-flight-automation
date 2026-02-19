dc8dbee17e96011cd465fdff25900680e33a66b9
Trinh Nguyen Tung <trinh.nguyen.tung@prudential.com.vn>
2026-02-19 23:20:22 +0700

    updated

--- utils/excelReader.ts
+++ utils/excelReader.ts
@@ -1,37 +1,27 @@
 import ExcelJS from 'exceljs';
 
 export async function readFlightData() {
-
   const workbook = new ExcelJS.Workbook();
-
   await workbook.xlsx.readFile('./test-data/flightData.xlsx');
-
   const sheet = workbook.getWorksheet('data');
-
   const data: any[] = [];
 
   if (!sheet) {
     throw new Error('Sheet "data" not found');
   }
 
-  // 🔥 lấy header row
   const headers: string[] = [];
-
   sheet.getRow(1).eachCell((cell, colNumber) => {
     headers[colNumber] = String(cell.value);
   });
 
-  // 🔥 đọc từng row theo header
   sheet.eachRow((row, rowNumber) => {
 
-    if (rowNumber === 1) return; // skip header
+    if (rowNumber === 1) return; 
 
     const rowData: any = {};
-
     row.eachCell((cell, colNumber) => {
       const key = headers[colNumber];
-
-      // auto mapping theo header
       rowData[key] = cell.value;
     });
 
@@ -39,6 +29,5 @@ export async function readFlightData() {
   });
 
   console.log('Loaded data:', data);
-
   return data;
 }
--- utils/excelToJson.ts
+++ utils/excelToJson.ts
@@ -1,36 +1,8 @@
-import ExcelJS from 'exceljs';
 import fs from 'fs';
+import { readFlightData } from './excelReader';
 
 async function convertExcelToJson() {
-
-  const workbook = new ExcelJS.Workbook();
-  await workbook.xlsx.readFile('./test-data/flightData.xlsx');
-
-  const sheet = workbook.getWorksheet('data');
-
-  const result: any[] = [];
-
-  // ⭐ lấy header row
-  const headers: string[] = [];
-
-  sheet?.getRow(1).eachCell((cell, colNumber) => {
-    headers[colNumber] = String(cell.value);
-  });
-
-  // ⭐ đọc data row
-  sheet?.eachRow((row, rowNumber) => {
-
-    if (rowNumber === 1) return;
-
-    const rowData: any = {};
-
-    row.eachCell((cell, colNumber) => {
-      const key = headers[colNumber];
-      rowData[key] = cell.value;
-    });
-
-    result.push(rowData);
-  });
+  const result = await readFlightData();
 
   fs.writeFileSync(
     './test-data/flightData.json',
